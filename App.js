bd668ccec4b2833185afaa4071d39822fb8a6ed4
Andrew Rydzak <Andrew.Rydzak@coxautoinc.com>
2020-06-05 07:23:29 -0500

    adjusted styling and logic, no delete button yet

--- App.js
+++ App.js
@@ -1,26 +1,31 @@
 // flexbox is the highly suggested for react native here, it is the deafult
 // for native
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import List from "./components/List";
 export default function App() {
-
-  // Data/Functionaly Used Const, othes below return statment
+  // Data/Functionaly Used Const, Functions and CSS After Return
   const [outputText, setOutputText] = useState("See Lemur?");
-  const [enteredGoal, setEnteredGoal] = useState('')
-  const [goals, setGoals] = useState([])
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+  let listItemCount = -1;
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello There My Kitties!</Text>
       <Button
         onPress={changeOutputText}
         title={outputText}
         style={buttonStyles.container}
       />
       {/* This is using flex direction */}
-      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+      <View style={viewStyles.main}>
         <TextInput
           onChangeText={updateInputText}
           id="textInput"
@@ -34,22 +39,33 @@ export default function App() {
           style={{ color: "#ff5c5c", fontWeight: "bold", padding: 30 }}
         />
       </View>
-      <View>
-          {
-            goals.map((goal)=><Text>{goal}</Text>)
-          }
-        </View>
+      <ScrollView
+        contentContainerStyle={[viewStyles.topDown, { marginVertical: 20 }]}
+      >
+        {goals.map((goal) => {
+          listItemCount += 1;
+          return (
+            <View key={listItemCount}>
+              <Text style={listItemStyles.main}>
+                {listItemCount}. {goal}
+              </Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 
-  // Functions Getters and Setters
- function updateInputText(enteredInputText){
-  setEnteredGoal(enteredInputText)
- }
- function addInputToList(){
-    setGoals([...goals, enteredGoal ])
- }
-
+  //-----------------------------Functions Getters and Setters
+  function updateInputText(enteredInputText) {
+    setEnteredGoal(enteredInputText);
+  }
+  function addInputToList() {
+    const trimEdEnteredGoal = enteredGoal.trim();
+    if (trimEdEnteredGoal !== null && trimEdEnteredGoal !== "") {
+      setGoals([...goals, enteredGoal]);
+    }
+  }
   function changeOutputText() {
     let toUpdateText = outputText;
     toUpdateText == "Ok Lemur!!"
@@ -58,8 +74,7 @@ export default function App() {
     setOutputText(toUpdateText);
   }
 }
-
-// Styles and CSS
+//-----------------------------Styles and CSS
 const color = "#ff5c5c";
 const textInputStyles = StyleSheet.create({
   main: {
@@ -70,16 +85,28 @@ const textInputStyles = StyleSheet.create({
     borderWidth: 1,
   },
 });
+const viewStyles = StyleSheet.create({
+  main: { flexDirection: "row", justifyContent: "center" },
+  topDown: { flexDirection: "column", justifyContent: "center", width: "100%" },
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginVertical: 20,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
 });
-
+const listItemStyles = StyleSheet.create({
+  main: {
+    margin: 10,
+    textAlign: "left",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
 const buttonStyles = StyleSheet.create({
   container: {
     flex: 1,
@@ -88,3 +115,4 @@ const buttonStyles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+//-----------------------------End
