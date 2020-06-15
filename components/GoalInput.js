510a5a188e2d2213f1407409c0ead6c7404d2511
Andrew Rydzak <Andrew.Rydzak@coxautoinc.com>
2020-06-15 06:02:11 -0500

    adjusted component binding to better pass function props

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -2,9 +2,7 @@ import React from "react";
 import { View, Button, StyleSheet, TextInput, Modal } from "react-native";
 
 export default function GoalInput(props) {
-
   return (
-
     <Modal visible={props.modalGoalsShown}>
       <View style={viewStyles.main}>
         {/* This is using flex direction */}
@@ -14,27 +12,31 @@ export default function GoalInput(props) {
           value={props.enteredGoal}
           style={textInputStyles.main}
         />
-        <Button 
-          title="Add"
-          color={'blue'}
-          onPress={props.addInputToList}
-        />
-        <Button 
+        <Button title="Add" color={"blue"} onPress={props.addInputToList} />
+        <Button
           title="Cancel"
           color={color}
-          onPress={props.setModalGoalsShown.bind(this, false)}
+          onPress={setModalViewFalse}
+          //---Apparently these 2 methods cause unecessary re-rendering
+          // onPress={()=>props.setModalGoalsShown(false)}
+          //---Whenever you use .bind() it’ll return a new function which will be bound to the object that we pass to the .bind.
+          // onPress={props.setModalGoalsShown.bind(this, false)}
+          //---Which means every time the Parent component re-renders the handler for the Child component will be a new function.
         />
       </View>
     </Modal>
   );
-  //--------------------------Helper Functions
+  //--------------------------Helper Functions That Live In The Component
+  function setModalViewFalse() {
+    props.setModalGoalsShown(false);
+  }
   function updateInputText(enteredInputText) {
     props.setEnteredGoal(enteredInputText);
   }
 }
 //-----------------------------Styles and CSS
 const viewStyles = StyleSheet.create({
-  main:  {justifyContent: "center", alignItems:'center', flex:1},
+  main: { justifyContent: "center", alignItems: "center", flex: 1 },
   topDown: { flexDirection: "column", justifyContent: "center", width: "100%" },
 });
 const color = "#ff5c5c";
